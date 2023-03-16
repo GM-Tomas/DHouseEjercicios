c74e89ea795f2280440c280c7225a8c3edde7ee9
TomasGM <tomasgonzalezm99@gmail.com>
2023-03-16 12:35:33 -0300

    Final edits.

--- Clase9/app.js
+++ Clase9/app.js
@@ -1,13 +1,13 @@
-const peliculas = require('./peliculas');
-const fs = require('fs');
-//const uuid = require('uuid');
+const peliculas = require("./peliculas");
+const fs = require("fs");
+const uuid = require("uuid");
 
-const msjPath='mensaje.txt';
+const msjPath = "mensaje.txt";
 
-peliculas.forEach(element => {
+peliculas.forEach((element) => {
   console.log(element);
 });
 
-const msjText = fs.readFileSync(msjPath, 'utf8');
+const msjText = fs.readFileSync(msjPath, "utf8");
 console.log(msjText);
-//console.log(uuid);
+console.log(uuid.v4());
