3d59f08ef409056385b9c22ef7ac31ae8648fa4d
TomasGM <tomasgonzalezm99@gmail.com>
2023-03-22 22:41:13 -0300

    Final ejercicio

--- Clase11/app.js
+++ Clase11/app.js
@@ -1,6 +1,25 @@
 const importar = require("./collectibles.js");
 
+let starWars = "starWars";
+let bandai = "bandai";
+let hotToys = "hotToys";
 
-//console.log(importar);
+let starWarsArray = importar(starWars);
+let bandaiArray = importar(bandai);
+let hotToysArray = importar(hotToys);
 
-console.log(importar('bandai'));
+let unifiedCollectibles = [...starWarsArray, ...bandaiArray, ...hotToysArray];
+
+const collectibles = {
+  figuras: unifiedCollectibles,
+  listFigures: function () {
+    for (const element of this.figuras) {
+      console.log(element);
+    }
+  },
+  figuresByBrand: function (marca) {
+    return this.figuras.filter((item) => item.marca == marca);
+  },
+};
+
+console.log(collectibles.figuresByBrand("Bandai"));
