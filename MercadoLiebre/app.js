468218fa7567af9c0c28d3702a48786c92b2388d
TomasGM <tomasgonzalezm99@gmail.com>
2023-04-23 02:41:43 -0300

    Express + microdesafio 1

---
+++ MercadoLiebre/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Configuración de directorio público
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ruta para la página principal
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'home.html'));
+});
+
+// Inicio del servidor
+app.listen(3000, () => {
+  console.log('Servidor en el puerto 3000');
+});
